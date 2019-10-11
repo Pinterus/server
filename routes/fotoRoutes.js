4b2675cf0eb50fcbf5d7d76a0f6b2e1f52a16323
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-10-11 11:25:37 +0700

    add add gcs upload server

--- routes/fotoRoutes.js
+++ routes/fotoRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const fotoController = require('../controllers/FotoController')
+const upload = require('../helpers/upload')
 
 router.get('/import', fotoController.import)
 router.get('/random', fotoController.getRandom)
 router.get('/search', fotoController.searchImages)
+router.post('/upload',  upload.multer.single('image'), upload.sendUploadToGCS, fotoController.getLinkUploaded)
 
 module.exports = router
